5be55343f071d07aba09f077a111dcc2c0929220
federicojasson <federicojasson@gmail.com>
2014-10-14 08:20:18 -0300

    14/10

---
+++ server/db/etrs.sql
@@ -0,0 +1,14 @@
+DELIMITER ;
+
+START TRANSACTION; -- Executes all statements or rollbacks
+
+source etrs_cleanup.sql
+source etrs_initialization.sql
+source etrs_tables.sql
+source etrs_views.sql
+source etrs_procedures.sql
+source etrs_triggers.sql
+source etrs_users.sql
+source etrs_debug_data.sql
+
+COMMIT; -- Commits the transaction
