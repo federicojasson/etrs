df8602a6fd0da75ac7a48e455803fba7175b4153
federicojasson <federicojasson@gmail.com>
2014-10-14 19:44:36 -0300

    14/10

--- server/db/etrs_cleanup.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER ;
-
--- Drops the database
-DROP DATABASE IF EXISTS etrs_database;
-
--- Drops the users of the database
-DROP USER etrs_doctor@localhost;
-DROP USER etrs_operator@localhost;
-DROP USER etrs_researcher@localhost;
