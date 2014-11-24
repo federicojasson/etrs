9a99fafbf3e5f3c2f9f0f76cff5a0e1b3d627103
federicojasson <federicojasson@gmail.com>
2014-11-24 14:32:43 -0300

    24/11

---
+++ client/scripts/forms/create-consultation-form.js
@@ -0,0 +1,103 @@
+// Uses strict mode in the whole script
+'use strict';
+
+(function() {
+	// Module: forms
+	var module = angular.module('forms');
+	
+	// Controller: CreateConsultationFormController
+	module.controller('CreateConsultationFormController', [
+		'builder',
+		'errorManager',
+		'server',
+		CreateConsultationFormController
+	]);
+	
+	/*
+	 * Controller: CreateConsultationFormController
+	 * 
+	 * Offers logic functions for the create consultation form.
+	 */
+	function CreateConsultationFormController(builder, errorManager, server) {
+		var controller = this;
+		
+		/*
+		 * TODO: comments
+		 */
+		var currentArea = 'data';
+		
+		/*
+		 * The TODO input model.
+		 */
+		var TODOInputModel = builder.buildInputModel(function() {
+			// TODO
+		});
+		
+		/*
+		 * Validates the form input and returns the result.
+		 */
+		function validate() {
+			// The form input is valid if all the input models are
+			var isValid = true;
+			
+			// Gets the input models
+			var inputModels = controller.inputModels;
+			
+			// Validates the input models
+			for (var property in inputModels) {
+				if (inputModels.hasOwnProperty(property)) {
+					// Validates the input model and ANDs the result
+					isValid &= inputModels[property].validate();
+				}
+			}
+			
+			return isValid;
+		}
+		
+		/*
+		 * The input models.
+		 */
+		controller.inputModels = {
+			// TODO
+		};
+		
+		/*
+		 * TODO: comments
+		 */
+		controller.isCurrentArea = function(area) {
+			return area === currentArea;
+		};
+		
+		/*
+		 * TODO: comments
+		 */
+		controller.setCurrentArea = function(area) {
+			currentArea = area;
+		};
+		
+		/*
+		 * Submits the form.
+		 * 
+		 * If any of the input models is invalid, the appropriate actions are
+		 * taken and the form is not submitted.
+		 */
+		controller.submit = function() {
+			if (! validate()) {
+				// At least one input model is invalid
+				return;
+			}
+			
+			// Gets the input values
+			var TODO = TODOInputModel.value;
+			
+			// Creates the consultation
+			server.createConsultation(TODO).then(function(response) {
+				// TODO
+			}, function(response) {
+				// Error: the server responded with an HTTP error
+				var error = builder.buildError(response);
+				errorManager.reportError(error);
+			});
+		};
+	}
+})();
