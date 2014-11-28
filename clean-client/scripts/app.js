5d1c97cbf470f07a5f3d449f9fb4edbca787c08b
federicojasson <federicojasson@gmail.com>
2014-11-27 23:51:12 -0300

    27/11

--- clean-client/scripts/app.js
+++ clean-client/scripts/app.js
@@ -55,13 +55,37 @@
 			url: '/'
 		});
 		
-		$stateProvider.state('default.index.private', {
+		$stateProvider.state('default.index.administrator', {
 			templateUrl: 'templates/views/index-view.html'
 		});
 		
-		$stateProvider.state('default.index.public', {
+		$stateProvider.state('default.index.anonymous', {
 			templateUrl: 'templates/views/log-in-view.html'
 		});
+		
+		$stateProvider.state('default.index.doctor', {
+			templateUrl: 'templates/views/index-view.html'
+		});
+		
+		$stateProvider.state('default.index.operator', {
+			templateUrl: 'templates/views/index-view.html'
+		});
+		
+		$stateProvider.state('default.log-in', {
+			controller: 'AccessController',
+			templateUrl: inclusionTemplate,
+			url: '/log-in'
+		});
+		
+		$stateProvider.state('default.log-in.anonymous', {
+			templateUrl: 'templates/views/log-in-view.html'
+		});
+		
+		
+		
+		
+		
+		
 		
 		$stateProvider.state('default.tasks', {
 			controller: 'AccessController',
@@ -69,13 +93,25 @@
 			url: '/tasks'
 		});
 		
-		$stateProvider.state('default.tasks.private', {
+		$stateProvider.state('default.tasks.administrator', {
 			templateUrl: 'templates/views/tasks-view.html'
 		});
 		
-		$stateProvider.state('default.tasks.public', {
+		$stateProvider.state('default.tasks.anonymous', {
 			templateUrl: 'templates/views/log-in-view.html'
 		});
+		
+		$stateProvider.state('default.tasks.doctor', {
+			templateUrl: 'templates/views/tasks-view.html'
+		});
+		
+		$stateProvider.state('default.tasks.operator', {
+			templateUrl: 'templates/views/tasks-view.html'
+		});
+		
+		
+		
+		
 		
 		$urlRouterProvider.otherwise('/');
 	}
@@ -88,6 +124,8 @@
 	function AccessController($state) {
 		// TODO: chequear si esta logueado
 		var loggedIn = false;
+		
+		console.log($state);
 		
 		if (loggedIn) {
 			$state.go('.private');
