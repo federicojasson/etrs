02cd87856bc9f94fb5cca672ba1985a9e95ce76c
federicojasson <federicojasson@gmail.com>
2014-12-12 01:11:30 -0300

    11/12

--- server/private/scripts/Helpers/BusinessLogicDatabase.php
+++ server/private/scripts/Helpers/BusinessLogicDatabase.php
@@ -14,13 +14,17 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				doppler_normal_neck_vessels,
-				nmr_decreased_cerebral_cortex,
-				nmr_decreased_hippocampal_volume,
-				nmr_frontotemporal_pattern,
-				nmr_vascular_pattern
-			FROM consultations_image_analysis
-			WHERE consultation = :consultationId
+				consultations_image_analysis.doppler_normal_neck_vessels AS dopplerNormalNeckVessels,
+				consultations_image_analysis.nmr_decreased_cerebral_cortex AS nmrDecreasedCerebralCortex,
+				consultations_image_analysis.nmr_decreased_hippocampal_volume AS nmrDecreasedHippocampalVolume,
+				consultations_image_analysis.nmr_frontotemporal_pattern AS nmrFrontotemporalPattern,
+				consultations_image_analysis.nmr_vascular_pattern AS nmrVascularPattern
+			FROM consultations_image_analysis INNER JOIN consultations_metadata
+			ON consultations_image_analysis.consultation = consultations_metadata.consultation
+			WHERE
+				NOT consultations_metadata.erased
+				AND
+				consultations_image_analysis.consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -43,17 +47,24 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				apo_e4,
-				calcium,
-				folic_acid,
-				phosphorus,
-				thyroids,
-				total_cholesterol,
-				triglycerides,
-				vdrl,
-				vitamin_b
-			FROM consultations_laboratory_results
-			WHERE consultation = :consultationId
+				consultations_laboratory_results.apo_e4 AS apoE4,
+				consultations_laboratory_results.b_vitamin AS bVitamin,
+				consultations_laboratory_results.c_reactive_protein AS cReactiveProtein,
+				consultations_laboratory_results.calcium AS calcium,
+				consultations_laboratory_results.d_vitamin AS dVitamin,
+				consultations_laboratory_results.folic_acid AS folicAcid,
+				consultations_laboratory_results.homocysteine AS homocysteine,
+				consultations_laboratory_results.phosphorus AS phosphorus,
+				consultations_laboratory_results.thyroids AS thyroids,
+				consultations_laboratory_results.total_cholesterol AS totalCholesterol,
+				consultations_laboratory_results.triglycerides AS triglycerides,
+				consultations_laboratory_results.vdrl as vdrl
+			FROM consultations_laboratory_results INNER JOIN consultations_metadata
+			ON consultations_laboratory_results.consultation = consultations_metadata.consultation
+			WHERE
+				NOT consultations_metadata.erased
+				AND
+				consultations_laboratory_results.consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -76,14 +87,18 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				date,
-				clinical_impression,
-				diagnosis,
-				indications,
-				observations,
-				reasons
-			FROM consultations_main_data
-			WHERE consultation = :consultationId
+				consultations_main_data.date AS date,
+				consultations_main_data.clinical_impression AS clinicalImpression,
+				consultations_main_data.diagnosis AS diagnosis,
+				consultations_main_data.indications AS indications,
+				consultations_main_data.observations AS observations,
+				consultations_main_data.reasons AS reasons
+			FROM consultations_main_data INNER JOIN consultations_metadata
+			ON consultations_main_data.consultation = consultations_metadata.consultation
+			WHERE
+				NOT consultations_metadata.erased
+				AND
+				consultations_main_data.consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -106,12 +121,15 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				creator,
-				patient,
-				erased,
-				creation_datetime
+				creator AS creator,
+				patient AS patient,
+				erased AS erased,
+				creation_datetime AS creationDatetime
 			FROM consultations_metadata
-			WHERE consultation = :consultationId
+			WHERE
+				NOT erased
+				AND
+				consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -134,9 +152,17 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				TODO: define fields
-			FROM consultations_neurocognitive_assessment
-			WHERE consultation = :consultationId
+				consultations_neurocognitive_assessment.addenbrooke_cognitive_examination AS addenbrookeCognitiveExamination,
+				consultations_neurocognitive_assessment.eye_tracking_pattern AS eyeTrackingPattern,
+				consultations_neurocognitive_assessment.ineco_frontal_screening AS inecoFrontalScreening,
+				consultations_neurocognitive_assessment.mini_mental_state_examination AS miniMentalStateExamination,
+				consultations_neurocognitive_assessment.working_memory_index AS workingMemoryIndex
+			FROM consultations_neurocognitive_assessment INNER JOIN consultations_metadata
+			ON consultations_neurocognitive_assessment.consultation = consultations_metadata.consultation
+			WHERE
+				NOT consultations_metadata.erased
+				AND
+				consultations_neurocognitive_assessment.consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -159,9 +185,20 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				TODO: check fields
-			FROM consultations_patient_background
-			WHERE consultation = :consultationId
+				consultations_patient_background.diabetes AS diabetes,
+				consultations_patient_background.dyslipidemia AS dyslipidemia,
+				consultations_patient_background.heart_disease AS heartDisease,
+				consultations_patient_background.hiv AS hiv,
+				consultations_patient_background.hypertension AS hypertension,
+				consultations_patient_background.psychiatric_disorder AS psychiatricDisorder,
+				consultations_patient_background.relative_with_alzheimer AS relativeWithAlzheimer,
+				consultations_patient_background.traumatic_brain_injury AS traumaticBrainInjury
+			FROM consultations_patient_background INNER JOIN consultations_metadata
+			ON consultations_patient_background.consultation = consultations_metadata.consultation
+			WHERE
+				NOT consultations_metadata.erased
+				AND
+				consultations_patient_background.consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -184,17 +221,21 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				antidepressants,
-				antidiabetics,
-				antihypertensives,
-				antiplatelets_anticoagulants,
-				antipsychotics,
-				benzodiazepines,
-				hypolipidemics,
-				levothyroxine,
-				melatonin
-			FROM consultations_patient_medications
-			WHERE consultation = :consultationId
+				consultations_patient_medications.antidepressants AS antidepressants,
+				consultations_patient_medications.antidiabetics AS antidiabetics,
+				consultations_patient_medications.antihypertensives AS antihypertensives,
+				consultations_patient_medications.antiplatelets_anticoagulants AS antiplateletsAnticoagulants,
+				consultations_patient_medications.antipsychotics AS antipsychotics,
+				consultations_patient_medications.benzodiazepines AS benzodiazepines,
+				consultations_patient_medications.hypolipidemics AS hypolipidemics,
+				consultations_patient_medications.levothyroxine AS levothyroxine,
+				consultations_patient_medications.melatonin AS melatonin
+			FROM consultations_patient_medications INNER JOIN consultations_metadata
+			ON consultations_patient_medications.consultation = consultations_metadata.consultation
+			WHERE
+				NOT consultations_metadata.erased
+				AND
+				consultations_patient_medications.consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -217,9 +258,20 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				TODO: check fields
-			FROM consultations_treatments
-			WHERE consultation = :consultationId
+				consultations_treatments.acetylcholinesterase_inhibitor AS acetylcholinesteraseInhibitor,
+				consultations_treatments.acetylsalicylic_acid AS acetylsalicylicAcid,
+				consultations_treatments.b_vitamin AS bVitamin,
+				consultations_treatments.concomitant_disease_referral AS concomitantDiseaseReferral,
+				consultations_treatments.folic_acid AS folicAcid,
+				consultations_treatments.memantine AS memantine,
+				consultations_treatments.neurocognitive_rehabilitation AS neurocognitiveRehabilitation,
+				consultations_treatments.physical_exercise AS physicalExercise
+			FROM consultations_treatments INNER JOIN consultations_metadata
+			ON consultations_treatments.consultation = consultations_metadata.consultation
+			WHERE
+				NOT consultations_metadata.erased
+				AND
+				consultations_treatments.consultation = :consultationId
 			LIMIT 1
 		';
 		
@@ -241,7 +293,7 @@ class BusinessLogicDatabase extends Database {
 	public function selectExperimentFiles($experimentId) {
 		// Defines the statement
 		$statement = '
-			SELECT file
+			SELECT file AS file
 			FROM experiments_files
 			WHERE experiment = :experimentId
 		';
@@ -265,8 +317,8 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				name,
-				command_line
+				name AS name,
+				command_line AS commandLine
 			FROM experiments_main_data
 			WHERE experiment = :experimentId
 			LIMIT 1
@@ -291,9 +343,9 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				creator,
-				erased,
-				creation_datetime
+				creator AS creator,
+				erased AS erased,
+				creation_datetime AS creationDatetime
 			FROM experiments_metadata
 			WHERE experiment = :experimentId
 			LIMIT 1
@@ -318,8 +370,8 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				hash,
-				name
+				hash AS hash,
+				name AS name
 			FROM files_main_data
 			WHERE file = :fileId
 			LIMIT 1
@@ -344,9 +396,9 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				creator,
-				erased,
-				creation_datetime
+				creator AS creator,
+				erased AS erased,
+				creation_datetime AS creationDatetime
 			FROM files_metadata
 			WHERE file = :fileId
 			LIMIT 1
@@ -371,11 +423,11 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				first_name,
-				last_name,
-				gender,
-				birth_date,
-				education_years
+				first_name AS firstName,
+				last_name AS lastName,
+				gender AS gender,
+				birth_date AS birthDate,
+				education_years AS educationYears
 			FROM patients_main_data
 			WHERE patient = :patientId
 			LIMIT 1
@@ -400,9 +452,9 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				creator,
-				erased,
-				creation_datetime
+				creator AS creator,
+				erased AS erased,
+				creation_datetime AS creationDatetime
 			FROM patients_metadata
 			WHERE patient = :patientId
 			LIMIT 1
@@ -426,7 +478,7 @@ class BusinessLogicDatabase extends Database {
 	public function selectStudyFiles($studyId) {
 		// Defines the statement
 		$statement = '
-			SELECT file
+			SELECT file AS file
 			FROM studies_files
 			WHERE study = :studyId
 		';
@@ -449,7 +501,7 @@ class BusinessLogicDatabase extends Database {
 	public function selectStudyMainData($studyId) {
 		// Defines the statement
 		$statement = '
-			SELECT observations
+			SELECT observations AS observations
 			FROM studies_main_data
 			WHERE study = :studyId
 			LIMIT 1
@@ -474,12 +526,12 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				consultation,
-				creator,
-				experiment,
-				report,
-				erased,
-				creation_datetime
+				consultation AS consultation,
+				creator AS creator,
+				experiment AS experiment,
+				report AS report,
+				erased AS erased,
+				creation_datetime AS creationDatetime
 			FROM studies_metadata
 			WHERE study = :studyId
 			LIMIT 1
@@ -504,8 +556,8 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				password_hash,
-				password_salt
+				password_hash AS passwordHash,
+				password_salt AS passwordSalt
 			FROM users_authentication_data
 			WHERE user = :userId
 			LIMIT 1
@@ -530,11 +582,11 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				first_name,
-				last_name,
-				gender,
-				email_address,
-				role
+				first_name AS firstName,
+				last_name AS lastName,
+				gender AS gender,
+				email_address AS emailAddress,
+				role AS role
 			FROM users_main_data
 			WHERE user = :userId
 			LIMIT 1
@@ -559,8 +611,8 @@ class BusinessLogicDatabase extends Database {
 		// Defines the statement
 		$statement = '
 			SELECT
-				erased,
-				creation_datetime
+				erased AS erased,
+				creation_datetime AS creationDatetime
 			FROM users_metadata
 			WHERE user = :userId
 			LIMIT 1
