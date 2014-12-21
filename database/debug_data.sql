105cd838d3b13722a985852069e9cc0ab1b452e1
federicojasson <federicojasson@gmail.com>
2014-12-21 15:48:21 -0300

    21/12

--- database/debug_data.sql
+++ database/debug_data.sql
@@ -64,6 +64,38 @@ INSERT INTO files (
 	UNHEX('8C7DD922AD47494FC02C388E12C00EAC')
 );
 
+INSERT INTO files (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name,
+	hash
+) VALUES (
+	UNHEX('6346a6dfce5540598544b65cbdcc2bf5'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'archivo1.pdf',
+	UNHEX('867DD9250347494FC021388E11500cae')
+);
+
+INSERT INTO files (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name,
+	hash
+) VALUES (
+	UNHEX('0f54836d16144515a9cbe3facd147545'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'archivo2.pdf',
+	UNHEX('117aa92502474900C0f1388E113b07a8')
+);
+
 INSERT INTO experiments (
 	id,
 	is_erased,
@@ -80,10 +112,132 @@ INSERT INTO experiments (
 	'programa.exe -f :input'
 );
 
-INSERT INTO consultations (
+INSERT into experiments_files (
+	experiment,
+	file
+) VALUES (
+	UNHEX('a16f4de5cf164c508fec3a1aa45455fd'),
+	UNHEX('0f54836d16144515a9cbe3facd147545')
+);
+
+INSERT INTO diagnoses (
 	id,
 	is_erased,
 	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('b6aff0cecc964aab848c3539f8c56f14'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'Demencia tipo Alzheimer'
+);
+
+INSERT INTO clinical_impressions (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('67f86454a18a4d2d88a7185b78e7aaae'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'Demencia'
+);
+
+INSERT INTO treatments (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('076ded42464b4a3cb4a284c6b969bd1e'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'Taller neurocognitivo'
+);
+
+INSERT INTO medications (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('5c7bafdf4aaf459f9eda4e6d991e03a4'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'Levotiroxina'
+);
+
+INSERT INTO backgrounds (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('fed917638d304431a12d4670af9bca9d'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'Cardiopatía'
+);
+
+INSERT INTO image_tests (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('0b7b260b05584ac992494ee548770b4e'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'RMN con patrón frontotemporal'
+);
+
+INSERT INTO neurocognitive_evaluations (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('9deea86b950e484f809e532d0a0196a4'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'MMSE'
+);
+
+INSERT INTO laboratory_tests (
+	id,
+	is_erased,
+	creator,
+	creation_datetime,
+	name
+) VALUES (
+	UNHEX('57f76e03c90445049f1e3222a17a72f0'),
+	FALSE,
+	'federicojasson',
+	UTC_TIMESTAMP(),
+	'Triglicéridos'
+);
+
+INSERT INTO consultations (
+	id,
+	is_erased,
+	clinical_impression,
+	creator,
+	diagnosis,
 	patient,
 	creation_datetime,
 	date,
@@ -93,13 +247,63 @@ INSERT INTO consultations (
 ) VALUES (
 	UNHEX('283db1dad6004f059e527bd9626a1f28'),
 	FALSE,
+	UNHEX('67f86454a18a4d2d88a7185b78e7aaae'),
 	'federicojasson',
+	UNHEX('b6aff0cecc964aab848c3539f8c56f14'),
 	UNHEX('1894068380304e0ca7ebf25d25e72dca'),
 	UTC_TIMESTAMP(),
 	'2013-05-01',
 	'Síntomas del paciente',
 	'',
 	''
+);
+
+INSERT INTO consultations_neurocognitive_evaluations (
+	consultation,
+	neurocognitive_evaluation
+) VALUES (
+	UNHEX('283db1dad6004f059e527bd9626a1f28'),
+	UNHEX('9deea86b950e484f809e532d0a0196a4')
+);
+
+INSERT INTO consultations_backgrounds (
+	consultation,
+	background
+) VALUES (
+	UNHEX('283db1dad6004f059e527bd9626a1f28'),
+	UNHEX('fed917638d304431a12d4670af9bca9d')
+);
+
+INSERT INTO consultations_image_tests (
+	consultation,
+	image_test
+) VALUES (
+	UNHEX('283db1dad6004f059e527bd9626a1f28'),
+	UNHEX('0b7b260b05584ac992494ee548770b4e')
+);
+
+INSERT INTO consultations_laboratory_tests (
+	consultation,
+	laboratory_test
+) VALUES (
+	UNHEX('283db1dad6004f059e527bd9626a1f28'),
+	UNHEX('57f76e03c90445049f1e3222a17a72f0')
+);
+
+INSERT INTO consultations_medications (
+	consultation,
+	medication
+) VALUES (
+	UNHEX('283db1dad6004f059e527bd9626a1f28'),
+	UNHEX('5c7bafdf4aaf459f9eda4e6d991e03a4')
+);
+
+INSERT INTO consultations_treatments (
+	consultation,
+	treatment
+) VALUES (
+	UNHEX('283db1dad6004f059e527bd9626a1f28'),
+	UNHEX('076ded42464b4a3cb4a284c6b969bd1e')
 );
 
 INSERT INTO studies (
@@ -120,4 +324,12 @@ INSERT INTO studies (
 	NULL,
 	UTC_TIMESTAMP(),
 	''
+);
+
+INSERT into studies_files (
+	study,
+	file
+) VALUES (
+	UNHEX('cb34783c21264abfb4fc0b371bd2b2fa'),
+	UNHEX('6346a6dfce5540598544b65cbdcc2bf5')
 );
