7ffd94267098494d66e0f2b5e91bd093c840b3c0
federicojasson <federicojasson@gmail.com>
2015-03-23 12:41:11 -0300

    request-sign-up action (partial)

--- client/app/action/request-sign-up/request-sign-up.js
+++ client/app/action/request-sign-up/request-sign-up.js
@@ -19,12 +19,5 @@
 'use strict';
 
 (function() {
-	angular.module('app.action', [
-		'app.action.createMedicine',
-		'app.action.editMedicine',
-		'app.action.requestPasswordReset',
-		'app.action.requestSignUp',
-		'app.action.signIn',
-		'app.action.signOut'
-	]);
+	angular.module('app.action.requestSignUp', []);
 })();
