3c085bfab2b614f2547f897309b95293bafd4540
federicojasson <federicojasson@gmail.com>
2015-03-28 01:07:52 -0300

    Permissions: delete-expired servicies defined

--- server/private/app/classes/Service/Permission/SignUp/DeleteExpired.php
+++ server/private/app/classes/Service/Permission/SignUp/DeleteExpired.php
@@ -18,27 +18,25 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace App\Middleware;
+namespace App\Service\Permission\SignUp;
 
 /**
- * Responsible for registering the internal services.
+ * Represents the /permission/sign-up/delete-expired service.
  */
-class InternalServices extends Services {
+class DeleteExpired extends \App\Service\Internal {
 	
 	/**
-	 * Returns the services.
+	 * Executes the service.
 	 */
-	protected function getServices() {
-		return [
-			// DEFINEHERE: define internal services here
-			'/data/check-configuration',
-			'/data/generate-proxies',
-			'/data/reset-entities-versions',
-			'/log/delete-old',
-			'/permission/password-reset/delete-expired',
-			'/permission/sign-up/delete-expired',
-			'/session/delete-expired'
-		];
+	protected function execute() {
+		// TODO
+	}
+	
+	/**
+	 * Determines whether the request is valid.
+	 */
+	protected function isRequestValid() {
+		return true;
 	}
 	
 }
