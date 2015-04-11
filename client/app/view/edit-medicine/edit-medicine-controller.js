10d70ad551118f35ed388548603a21c8d53285ac
federicojasson <federicojasson@gmail.com>
2015-04-11 18:13:22 -0300

    comment changed

--- client/app/view/edit-medicine/edit-medicine-controller.js
+++ client/app/view/edit-medicine/edit-medicine-controller.js
@@ -91,7 +91,7 @@
 			// Initializes the action
 			var action = new EditMedicineAction();
 			
-			// Sets the inputs' initial values
+			// Sets inputs' initial values
 			action.input.id.value = medicine.id;
 			action.input.version.value = medicine.version;
 			action.input.name.value = medicine.name;
