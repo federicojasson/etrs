6fd1e55e4bb02e960d2dab0fd945296ec4bd83e9
federicojasson <federicojasson@gmail.com>
2015-04-21 17:53:24 -0300

    GetAll services

---
+++ server/private/app/classes/Service/ImagingTest/GetAll.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * ETRS - Eye Tracking Record System
+ * Copyright (C) 2015 Federico Jasson
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Service\ImagingTest;
+
+/**
+ * Represents the /imaging-test/get service.
+ */
+class GetAll extends \App\Service\External {
+	
+	/**
+	 * Executes the service.
+	 */
+	protected function execute() {
+		global $app;
+		
+		// Gets all imaging tests
+		$imagingTests = $app->data->createQueryBuilder()
+			->select('it.id')
+			->from('Entity:ImagingTest', 'it')
+			->getQuery()
+			->getResult();
+		
+		// Sets an output
+		$this->setOutputValue('ids', $imagingTests, createArrayFilter(function($imagingTest) {
+			return bin2hex($imagingTest['id']);
+		}));
+	}
+	
+	/**
+	 * Determines whether the request is valid.
+	 */
+	protected function isRequestValid() {
+		return true;
+	}
+	
+	/**
+	 * Determines whether the user is authorized.
+	 */
+	protected function isUserAuthorized() {
+		global $app;
+		
+		// Validates the access
+		return $app->accessValidator->isUserAuthorized([
+			USER_ROLE_ADMINISTRATOR,
+			USER_ROLE_DOCTOR
+		]);
+	}
+
+}
---
+++ server/private/app/classes/Service/Medicine/GetAll.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * ETRS - Eye Tracking Record System
+ * Copyright (C) 2015 Federico Jasson
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Service\Medicine;
+
+/**
+ * Represents the /medicine/get service.
+ */
+class GetAll extends \App\Service\External {
+	
+	/**
+	 * Executes the service.
+	 */
+	protected function execute() {
+		global $app;
+		
+		// Gets all medicines
+		$medicines = $app->data->createQueryBuilder()
+			->select('m.id')
+			->from('Entity:Medicine', 'm')
+			->getQuery()
+			->getResult();
+		
+		// Sets an output
+		$this->setOutputValue('ids', $medicines, createArrayFilter(function($medicine) {
+			return bin2hex($medicine['id']);
+		}));
+	}
+	
+	/**
+	 * Determines whether the request is valid.
+	 */
+	protected function isRequestValid() {
+		return true;
+	}
+	
+	/**
+	 * Determines whether the user is authorized.
+	 */
+	protected function isUserAuthorized() {
+		global $app;
+		
+		// Validates the access
+		return $app->accessValidator->isUserAuthorized([
+			USER_ROLE_ADMINISTRATOR,
+			USER_ROLE_DOCTOR
+		]);
+	}
+
+}
