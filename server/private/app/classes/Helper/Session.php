8ddc5ba38beb5fc8240c38d06e8f2187c8b2efd8
federicojasson <federicojasson@gmail.com>
2015-04-26 14:51:33 -0300

    TODO removed

--- server/private/app/classes/Helper/Session.php
+++ server/private/app/classes/Helper/Session.php
@@ -41,7 +41,7 @@ class Session {
 		// Configures the session cookie
 		ini_set('session.cookie_lifetime', SESSION_MAXIMUM_AGE * 24 * 60 * 60);
 		ini_set('session.cookie_path', $server['path']);
-		//ini_set('session.cookie_domain', $server['domain']); // TODO: not working in Chrome?
+		ini_set('session.cookie_domain', $server['domain']);
 		ini_set('session.cookie_secure', true);
 		ini_set('session.cookie_httponly', true);
 		
