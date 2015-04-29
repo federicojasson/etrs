43bf9c5c8704880653630bc07fe8f63ade2187ac
federicojasson <federicojasson@gmail.com>
2015-04-29 18:03:59 -0300

    edit-experiment ready

--- client/app/view/edit-experiment/edit-experiment-controller.js
+++ client/app/view/edit-experiment/edit-experiment-controller.js
@@ -68,7 +68,11 @@
 			var id = $stateParams.id;
 			
 			// Resets the data service
-			data.reset();
+			data.reset(1, {
+				Experiment: [
+					'files'
+				]
+			});
 			
 			// Gets the experiment
 			data.getExperiment(id).then(function(experiment) {
