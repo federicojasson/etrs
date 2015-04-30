df953e1ab5d914ce4174944c8dd12d4f042bd3d6
federicojasson <federicojasson@gmail.com>
2015-04-30 14:28:34 -0300

    tableRowLink directive

--- client/app/utility/table-row-link/table-row-link.js
+++ client/app/utility/table-row-link/table-row-link.js
@@ -19,17 +19,5 @@
 'use strict';
 
 (function() {
-	angular.module('app.utility', [
-		'app.utility.age',
-		'app.utility.date',
-		'app.utility.fullName',
-		'app.utility.gender',
-		'app.utility.honorificName',
-		'app.utility.integer',
-		'app.utility.levelName',
-		'app.utility.line',
-		'app.utility.nullIfEmpty',
-		'app.utility.role',
-		'app.utility.tableRowLink'
-	]);
+	angular.module('app.utility.tableRowLink', []);
 })();
